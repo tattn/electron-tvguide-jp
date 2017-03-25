3db5b98928eb06bc66423c7c670b009e694637f1
tattn (Tatsuya Tanaka) <tanakasan2525@gmail.com>
2017-03-26 05:37:21 +0900

    Fix release script

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 mkdir -p dist
 
-electron-packager src TVGuide --platform=all --arch=all --electron-version=1.6.2 --out dist/ --overwrite
+# electron-packager src TVGuide --platform=all --arch=all --electron-version=1.6.2 --out dist/ --overwrite
 
 mkdir -p dist/releases
+rm -rf dist/releases/*
 
-pushd dist
+pushd dist > /dev/null
 for file in	*; do
 	if [ ${file} = "releases" ]; then
 		continue
 	fi
 	echo "${file}"
+	echo "releases/${file}.zip"
 	zip -r releases/${file}.zip ${file}
 done
-popd
+popd > /dev/null
